a8c33e90bc6bd128f6b06cc692ced9765b959ae5
xuyuanfeng <986771570@qq.com>
2019-09-28 11:12:09 +0800

    [update]

--- CombatTracker.lua
+++ CombatTracker.lua
@@ -3,10 +3,8 @@ local end_time = 0
 local total_time =0
 local total_damage=0
 local averange_dps= 0 
-ChatFrame1:AddMessage("欢迎使用CombatTracker！！")
+ChatFrame1:AddMessage("欢迎使用TrackerX！！")
 function CombatTracker_OnEvent(frame, event, ...)
-    ChatFrame1:AddMessage("ON_EVENT被触发了")
-    ChatFrame1:AddMessage(tostring(event))
     if event == "PLAYER_REGEN_ENABLED" then 
         ChatFrame1:AddMessage("离开了战斗....")
         CombatTrackerFrameText:SetText("离开战斗...")
@@ -15,44 +13,28 @@ function CombatTracker_OnEvent(frame, event, ...)
         averange_dps =total_damage /total_time
         CombatTracker_UpdateText()
     elseif event == "PLAYER_REGEN_DISABLED" then 
-        ChatFrame1:AddMessage("im combat ...")
-        CombatTrackerFrameText:SetText("战斗状态")
+        CombatTrackerFrameText:SetText("进入战斗状态")
         total_damage = 0 
         start_time=GetTime()
     elseif event == "UNIT_COMBAT" then 
         local unit,action,modifier,damage,damagetype = ...
-        printMsg(unit)
-        printMsg(action)
-        printMsg(modifier)
-        printMsg(damage)
-        printMsg(damagetype)
-        total_damage=total_damage+damage
-        end_time = GetTime()
-        total_time = end_time - start_time
-        averange_dps =total_damage/total_time
-        CombatTracker_UpdateText()
-    -- elseif event =="UNIT_COMBAT" then 
-    --   if not InCombatLockdown()qqsqsqsq then 
-    --     -- 什么也不做
-    --   else then
-    --      local unit,action,modifier,damage,damagetype = ...
-    --      if unit == "player" and action == "HEAL" then 
-    --       total_damage =total_damage +damage
-    --       end_time =GetTime()
-    --       total_time =end_time -start_time
-    --       averange_dps = total_damage / total_time
-    --       CombatTracker_UpdateText()
+        if unit == "player" and action ~= "HEAL" then
+          total_damage=total_damage+damage
+          end_time = GetTime()
+          total_time = end_time - start_time
+          averange_dps =total_damage/total_time
+          CombatTracker_UpdateText()
+        end 
     end 
 end
 function CombatTracker_UpdateText()
-   local status = string.format( "%ds /d dmg /%.2f dps",total_time,total_damage,averange_dps)
+   local status = string.format( "%d秒/ %d总量/ %.2f平均伤害值",total_time,total_damage,averange_dps)
    CombatTrackerFrameText:SetText(status)
 end 
 function CombatTracker_ReportDPS()
-    ChatFrame1:AddMessage("load... CombatTracker_ReportDPS");
-    local msgformat = "%d seconds spent in combat with %d incoming damage .Average incoming DSP was %.2f"
+    local msgformat = "战斗时长%d 秒,受到了%d点伤害 .平均每秒受到的伤害:%.2f"
     local msg =string.format(msgformat,total_time,total_damage,averange_dps)
-    ChatFrame1:AddMessage(tostring(msg));
+    ChatFrame1:AddMessage(tostring(msg))
     if GetNumPartyMembers()>0 then 
       SendChatMessage(msg,"PARTY")
     else 
